12857eed40d56c69c021ca3321b71973e57addf4
UncomfortableChai <mj1055119@gmail.com>
2022-12-08 18:09:02 -0800

    updating the js

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const inputs = document.querySelectorAll('input');
 
-const cash = new Map();
+let total = 0;
 
-cash.set([1, 0]);
-cash.set([5, 0]);
-cash.set([10, 0]);
-cash.set([25, 0]);
-cash.set([100, 0]);
-cash.set([500, 0]);
-cash.set([1000, 0]);
-cash.set([2000, 0]);
-cash.set([5000, 0]);
-cash.set([10000, 0]);
+const cash = {
+    pennies: 0,
+    nickles: 0,
+    dimes: 0,
+    quarters: 0,
+    "five dollars": 0,
+    "ten dollars": 0,
+    "twenty dollars": 0,
+    "fifty dollars": 0,
+    "one hundred dollars": 0,
+};
 
 inputs.forEach((each) => {
-    each.addEventListener('input', (e) => console.log(console.log(e.target.value * parseInt(e.target.dataset.value))))
+    each.addEventListener('input', (e) => updateTotal(e))
 });
+
+function updateTotal(e) {
+    const dataSet = e.target.dataset;
+    cash[dataSet.name] = e.target.value * parseInt(dataSet.value);
+    // console.log(e.target.value * parseInt(dataSet.value));
+
+    Object.keys(cash).forEach((key) => console.log(cash[key]));
+
+    console.log(cash);
+    console.log(total);
+};
+
+Object.keys(cash).forEach((key) => console.log(cash[key]));
+
+console.log(Object.keys(cash));
